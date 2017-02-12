f1d1e650b8c9ae5d688b9fa4e4020083b27c14aa
Malcolm Littlefield <mhlittlefield@gmail.com>
2017-02-12 18:48:18 -0500

    Added tools.php and submit button

--- index.php
+++ index.php
@@ -2,9 +2,26 @@
 <html lang="en">
 <head>
     <title>Bill Splitter</title>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/darkly/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Bill Splitter</h1>
+    
+    <form method="get" action="index.php">
+      <input type="submit" class="btn btn-primary">
+    </form>
+
+
+    <!--text input to get the bill amount-->
+
+    <!--text input for tip percentage-->
+
+    <!--dropdown menu for quality of service-->
+
+    <!--checkbox to round up?-->
+
+
 </body>
 </html>
